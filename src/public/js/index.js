95da2204844310255c7f0ab2a1be6c8d679da897
fiorella buccolo <fiobuccolo@gmail.com>
2023-07-11 13:03:02 +0200

    customer attributes

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -40,10 +40,23 @@ class User{
         const planType = document.getElementById("data-plan-type").value;
         console.log(`tipo: ${planType}`)  
         const planAmount = document.getElementById("data-plan-amount").value;
-        const planFrecuency = document.getElementById("data-plan-frecuency").value;
+        const planFrequency = document.getElementById("data-plan-frequency").value;
         const planCycles = document.getElementById("data-plan-billying-cycles").value;
         const planPaymentDay = document.getElementById("data-plan-payment-day").value;
+      //   ---- customer attributes -----
+        const customerEmail = document.getElementById("data-customer-email").value;
+        const customerCif = document.getElementById("data-customer-cif").value;
+        const customerName = document.getElementById("data-name").value;
+        const customerSurname = document.getElementById("data-surname").value;
+        const customerPrefix = document.getElementById("data-prefix").value;
+        const customerPhone = document.getElementById("data-phone").value;
+        const customerCountry = document.getElementById("data-country").value;
+        const customerCity = document.getElementById("data-city").value;
+        const customerPostalCode = document.getElementById("data-postal-code").value;
+        const customerAddress = document.getElementById("data-address").value;
+        const customerExternalId = document.getElementById("data-external-customer-id").value;
         let editable = document.getElementById("editable").checked;
+        // -------
         console.log(serviceName)
         console.log(`editable: ${editable}`)
         const plan = new Plan(serviceName,planName,planDescription,planAmount);
@@ -53,10 +66,24 @@ class User{
         nuevoBotonUelz.setAttribute("data-plan-amount", planAmount)
         if (planType) {nuevoBotonUelz.setAttribute("data-plan-type", planType)}
             else{nuevoBotonUelz.setAttribute("data-plan-type", "unique")};
-        if(planFrecuency) nuevoBotonUelz.setAttribute("data-plan-frecuency", planFrecuency)
+        if(planFrequency) nuevoBotonUelz.setAttribute("data-plan-frecuency", planFrequency)
         if(planCycles) nuevoBotonUelz.setAttribute("data-plan-frecuency", planCycles)
         if(planPaymentDay) nuevoBotonUelz.setAttribute("data-plan-frecuency", planPaymentDay)
-        nuevoBotonUelz.setAttribute("data-customer-not-editable",editable)
+        if(customerEmail) nuevoBotonUelz.setAttribute("data-customer-email", customerEmail)
+        if(customerCif) nuevoBotonUelz.setAttribute("data-customer-cif", customerCif)
+        if(customerName) nuevoBotonUelz.setAttribute("data-name", customerName)
+        if(customerSurname) nuevoBotonUelz.setAttribute("data-surname", customerSurname)
+        if(customerPrefix) nuevoBotonUelz.setAttribute("data-prefix", customerPrefix)
+        if(customerPhone) nuevoBotonUelz.setAttribute("data-phone", customerPhone)
+        if(customerCountry) nuevoBotonUelz.setAttribute("data-country", customerCountry)
+        if(customerCity) nuevoBotonUelz.setAttribute("data-city", customerCity)
+        if(customerPostalCode) nuevoBotonUelz.setAttribute("data-postal-code", customerPostalCode)
+        if(customerAddress) nuevoBotonUelz.setAttribute("data-addres", customerAddress)
+        if(customerExternalId) nuevoBotonUelz.setAttribute("data-external-customer-idl", customerExternalId)
+        if(editable=false)
+            {nuevoBotonUelz.removeAttribute("data-customer-not-editable")}else{
+               nuevoBotonUelz.setAttribute("data-customer-not-editable")
+            }
 
 
          
